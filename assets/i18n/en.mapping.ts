3fff59bc20066238a8184aa62931dbdce2f9aaf4
Gayathri <k.gayathri344@gmail.com>
2023-11-26 22:41:13 +0530

    Updates

--- assets/i18n/en.mapping.ts
+++ assets/i18n/en.mapping.ts
@@ -2,6 +2,7 @@ export const EN_MAPPING = {
   COMMON: {
     ABOUT: 'About',
     AREA: 'Area',
+    BSNL_CONNECT: 'BSNL Connect',
     CABLE: 'Cable',
     DASHBOARD: 'Dashboard',
     HIDE_ADVANCED_FILTERS: 'Hide Advanced Filters',
