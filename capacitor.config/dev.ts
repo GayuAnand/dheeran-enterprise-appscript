098352ba27bb7766bbdddbba63cfdd36b2edbef8
Gayathri <k.gayathri344@gmail.com>
2024-02-12 20:44:57 +0530

    Update
    Signed-off-by: Gayathri <k.gayathri344@gmail.com>

---
+++ capacitor.config/dev.ts
@@ -0,0 +1,21 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.depco.management',
+  appName: 'depco-management',
+  webDir: 'dist/cap-webview',
+  // server: {
+  //   androidScheme: 'https'
+  // },
+  server: {
+    url: 'http://192.168.29.67:8200', // ifconfig => en0 -> inet
+    cleartext: true
+  },
+  plugins: {
+    CapacitorUpdater: {
+      autoUpdate: false,
+    }
+  }
+};
+
+export default config;
